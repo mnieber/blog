ff23a706b44de3ebbe6ec841d8ee931def87b29a
Maarten Nieber <hallomaarten@yahoo.com>
2023-06-16 22:08:29 +0200

    Fix apostroph problem

--- frontend/src/frames/components/AboutMeView.tsx
+++ frontend/src/frames/components/AboutMeView.tsx
@@ -1,9 +1,9 @@
 export const AboutMeView = () => {
   return (
     <div>
-      I'm an all-round programmer of front-ends, back-ends, desktop apps and
+      {`I'm an all-round programmer of front-ends, back-ends, desktop apps and
       tools (using Python/Javascript/C++) who believes purism and pragmatism can
-      go hand in hand: dream big first, then trim to size and start coding.
+      go hand in hand: dream big first, then trim to size and start coding.`}
     </div>
   );
 };
